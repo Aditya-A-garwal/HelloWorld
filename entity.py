f38c9fb334fb04c59047dd0655a471ad5b07ddcb
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2020-11-02 22:07:53 +0530

    Aditya: Moved ahead on implementation of event handler

--- entity.py
+++ entity.py
@@ -216,8 +216,12 @@ class Inventory:
         self.items      = [ [ None for j in range( 0, cols ) ] for i in range( 0, rows ) ]
         self.quantities = [ [ 0 for j in range( 0, cols ) ] for i in range( 0, rows ) ]
         self.positions  = {}
+
+        self.ITEM_TABLE_LOCAL = []
+
         self.selPos     = 0
         self.selItem    = [None, 0]
+
         self.itemHeld   = None
 
         self.isEnabled = False
@@ -225,23 +229,34 @@ class Inventory:
     def addItem( self, i:int, q:int ):
         pass
 
-    def addItemPos( self, i:int, q:int, p ):
-        if self.items[p[1]][p[0]] != i:
-            self.selItem[0] = self.items[p[1]][p[0]]
-            self.selItem[1] = self.quantities[p[1]][p[0]]
-            self.items[p[1]][p[0]] = i
-            self.quantities[p[1]][p[0]] = q
+    def addItemPos( self, item, quantity:int, pos ):
+
+        if( self.items[pos[1]][pos[0]] != item ):
+
+            self.selItem[0] = self.items[ pos[1] ][ pos[0] ]
+            self.selItem[1] = self.quantities[ pos[1] ][ pos[0] ]
+
+            self.items[ pos[1] ][ pos[0] ] = item
+            self.quantities[ pos[1] ][ pos[0] ] = quantity
+
         else:
-            if self.quantities[p[1]][p[0]] + q > items.ITEM_ATTR[i][MAX_STACK]:
-                self.selItem[0] = i
-                self.selItem[1] = self.quantities[p[1]][p[0]] + q - items.ITEM_ATTR[i][MAX_STACK]
-                self.quantities[p[1]][p[0]] = items.ITEM_ATTR[i][MAX_STACK]
+
+            self.items[pos[1]][pos[0]] = item
+            self.items[pos[1]][pos[0]] = item
+
+            if( self.quantities[pos[1]][pos[0]] + quantity > items.ITEM_ATTR[i][MAX_STACK] ):
+
+                self.selItem[0] = item
+                self.selItem[1] = self.quantities[pos[1]][pos[0]] + quantity - items.ITEM_ATTR[i][MAX_STACK]
+
+                self.quantities[pos[1]][pos[0]] = items.ITEM_ATTR[i][MAX_STACK]
 
             else:
+
                 self.selItem[0] = None
                 self.selItem[1] = 0
-                self.quantities[p[1]][p[0]] += q
-            self.items[p[1]][p[0]] = i
+
+                self.quantities[pos[1]][pos[0]] += quantity
 
     def addItemLast( self, i, q ):
         pass
@@ -256,6 +271,66 @@ class Inventory:
         pass
 
 class WorldEventHandler:
+    """ Class to abstract recording, management and processing of client-side events
+
+        Types of events
+
+        1> Camera movement event:
+            Description:
+                This event is triggered when the camera moved between 2 consecutive frames.
+                It requires the whole screen to be reloaded.
+                It requires re-calculation of the cursor position.
+            Associated Data:
+                - position of the mouse pointer in the updated window
+
+        2> User Keyboard input event:
+            Description:
+                This event is triggered when the user gives input from the keyboard, i.e. presses or releases keys.
+                It may open the console for issuing text commands, open the chat-box for typing messages or cause
+                the player to perform an action.
+            Associated Data:
+                - The key(s) which were pressed (if any) and the key(s) which were released (if any)
+
+        3> User mouse button input event:
+            Description:
+                This event is triggered when the user gives input from the mouse buttons but does not include the
+                movement of the mouse pointer. It might require either the whole screen or a part to be reloaded
+            Associated Data:
+                - The button(s) which were pressed (if any) and the button(s) which were released (if any)
+                - The changed regions of the chunk (in case of blocks broken, placed or altered
+                - The entity being affected, and all associated region data
+
+        4> User mouse pointer input event:
+            Description:
+                This event is triggered when the user moves the mouse pointer on the window.
+                It requires re-calculation of the cursor position.
+
+        5> Window resize event
+            Description:
+                This event is triggered when the window is resized.
+                It requires the whole screen to be reloaded.
+                It requires re-calculation of the cursor position.
+            Associated Data:
+                - The new size of the window
+                - position of the mouse pointer in the resized window
+
+        6> tile/wall break event
+            Description:
+            Associated Data:
+
+        7> tile/wall place event
+            Description:
+            Associated Data:
+
+        8> tile/wall alter event
+            Description:
+            Associated Data:
+
+        9> chunk-shifting event
+            Description:
+            Associated Data:
+
+    """
 
     def __init__( self ):
 
@@ -308,7 +383,7 @@ class WorldEventHandler:
         self.mouseState[ button ] = False
 
     def addVideoResize( self ):
-        print("VIDEO RESIZE")
+        #print("VIDEO RESIZE")
         vidResizeFlag = True
 
     def addCameraMotion( self ):
@@ -322,5 +397,14 @@ class EntityBuffer:
         self.length = 0
         self.len = 0
 
-        self.entities = [ ]
+        self.entities = { }
         self.mousePos       =   [0, 0]
+
+    def shiftLeftLoadRight( self ):
+        pass
+
+    def shiftRightLoadLeft( self ):
+        pass
+
+    def saveComplete( self ):
+        pass
